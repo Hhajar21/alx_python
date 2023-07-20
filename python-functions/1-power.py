3288bf99c01580a6e77ac2a81d2ad5a996265e71
Hhajar21 <hajarhabibi016@gmail.com>
2023-07-20 19:21:26 +0200

    1-power.pyy

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,16 +1,20 @@
 def pow(a, b):
-    if b == 0:
-        return 1
+    if a == 0 and b < 0:
+        raise ValueError("Cannot raise 0 to a negative power")
+
+    result = 1
 
     if b < 0:
         a = 1 / a
         b = -b
 
-    result = 1
     while b > 0:
         if b % 2 == 1:
             result *= a
         a *= a
         b //= 2
+
+    # Round the result to 15 decimal places for better precision
+    result = round(result, 15)
 
     return result
