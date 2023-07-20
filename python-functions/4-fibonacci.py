599dbde866da83c784679e67f9999251e87ffc25
Hhajar21 <hajarhabibi016@gmail.com>
2023-07-20 19:13:57 +0200

    4-fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -11,10 +11,3 @@ def fibonacci_sequence(n):
             next_num = sequence[i-1] + sequence[i-2]
             sequence.append(next_num)
         return sequence
-
-fibonacci_sequence = __import__('fibonacci_sequence').fibonacci_sequence
-
-print(fibonacci_sequence(10))
-print(fibonacci_sequence(5))
-print(fibonacci_sequence(0))
-print(fibonacci_sequence(1))
