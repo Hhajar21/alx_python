bf5501b5b657501a860c20191d66a861395d5679
Hhajar21 <hajarhabibi016@gmail.com>
2023-07-20 18:58:26 +0200

    1-main.py

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,2 +1,14 @@
 def pow(a, b):
-    return a ^ b
+    result = 1
+
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+
+    return result
