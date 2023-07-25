25d1aa78fcb9d17da2375094c4afb1e7ea084077
Hhajar21 <hajarhabibi016@gmail.com>
2023-07-25 19:44:38 +0200

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-if __name__ == "__main__":
-    add_module = __import__('add_0')
-    add = add_module.add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+from add_0 import add
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
