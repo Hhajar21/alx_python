212e7a322962b0904a5d4fe6666d6777797ae2a3
Hhajar21 <hajarhabibi016@gmail.com>
2023-07-29 15:14:43 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,23 @@
-class Square :
-    def __init__(self, size) :
-        self.__size=size
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size):
+            Initializes a square object with a given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square object with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            None.
+        """
+        self.__size = size
