bf0dc020387a0bc40e32ff866b96346d1e0b1cd3
Hhajar21 <hajarhabibi016@gmail.com>
2023-08-02 18:52:20 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,16 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
 class Square:
-    """
-    This class represents a square.
-
+    """Represents a square
     Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size):
-            Initializes a square object with a given size.
+        __size (int): size of a side of the square
     """
-
     def __init__(self, size):
-        """
-        Initializes a square object with a given size.
-
+        """Initializes a square
         Args:
-            size (int): The size of the square.
-
-        Raises:
-            None.
+            size (int): size of a side of the square
+        Returns: None
         """
         self.__size = size
