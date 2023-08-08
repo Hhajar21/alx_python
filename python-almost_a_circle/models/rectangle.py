c09298ef0d8191a1fcc9a0177595e617cc402d3a
Hhajar21 <hajarhabibi016@gmail.com>
2023-08-08 12:04:30 +0200

    rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+"""
+This is the 'rectangle' module.
+
+This module contains the Rectangle class which inherits from the Base class
+and represents a rectangle with width, height, x, and y attributes.
+
+"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    The Rectangle class, a subclass of Base.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle's position.
+        y (int): The y-coordinate of the rectangle's position.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position.
+            y (int, optional): The y-coordinate of the rectangle's position.
+            id (int, optional): The ID for the instance.
+
+        Returns:
+            None
+
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y."""
+        self.__y = value
