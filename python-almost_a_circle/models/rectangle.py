2e9fdc224575dd7a6be37cb68c2a66ebb1eeef30
Hhajar21 <hajarhabibi016@gmail.com>
2023-08-08 12:16:10 +0200

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,92 +21,7 @@ class Rectangle(Base):
         y (int): The y-coordinate of the rectangle's position.
     """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initializes a new Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle's position.
-            y (int, optional): The y-coordinate of the rectangle's position.
-            id (int, optional): The ID for the instance.
-
-        Returns:
-            None
-
-        """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        """Getter for width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter for width."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Getter for height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter for height."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter for x."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter for x."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter for y."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter for y."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """
-        Calculate the area of the Rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-
-        """
-        return self.__width * self.__height
+    # ... (previous code)
 
     def display(self):
         """
@@ -116,16 +31,9 @@ class Rectangle(Base):
             None
 
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
-    def __str__(self):
-        """
-        Return a string representation of the Rectangle.
-
-        Returns:
-            str: The string representation of the Rectangle.
-
-        """
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height)
+    # ... (previous code)
