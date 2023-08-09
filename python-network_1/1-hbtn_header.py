0df23e13f01f9166df5fa7498373e43ff167bce7
Hhajar21 <hajarhabibi016@gmail.com>
2023-08-09 13:58:33 +0200

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""
-Fetches the value of the X-Req-ID header from the url given 
+"""Fetches the value of the X-Request-Id header from a given URL's response"""
 
-"""
 import requests
 import sys
 
 def main():
-    """Main function to fetch and display The X-req-Id header"""
+    """Main function to fetch and display the X-Request-Id header"""
     if len(sys.argv) != 2:
         print("Usage: ./1-hbtn_header.py <URL>")
         sys.exit(1)
 
-
     url = sys.argv[1]
     response = requests.get(url)
-    x_req_id =response.headers.get('X-Req-Id')
+    x_request_id = response.headers.get('X-Request-Id')
 
-    if x_req_id is not None:
-        print(x_req_id)
+    if x_request_id is not None:
+        print(x_request_id)
 
 if __name__ == "__main__":
-    main()    
-
+    main()
