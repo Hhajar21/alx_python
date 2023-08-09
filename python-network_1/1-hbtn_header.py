e57c275c9640a032bb9838c83e0a8b11e13ad82e
Hhajar21 <hajarhabibi016@gmail.com>
2023-08-09 13:56:49 +0200

    1-hbtn_header.py

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Fetches the value of the X-Req-ID header from the url given 
+
+"""
+import requests, sys
+
+def main():
+    """Main function to fetch and display The X-req-Id header"""
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
+
+
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_req_id =response.headers.get('X-Req-Id')
+
+    if x_req_id is not None:
+        print(x_req_id)
+
+if __name__ == "__main__":
+    main()    
+
