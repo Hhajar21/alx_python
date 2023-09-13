7c364d3d32bb25af12b782db9d28a7eaf204a00e
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-13 17:21:07 +0200

    2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,7 @@
-"""
-This module checks if an object is the instance of a class
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
 
-"""
+
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
 
