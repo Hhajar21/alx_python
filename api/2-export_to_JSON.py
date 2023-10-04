4f8669736ee4bb9ea75ace6550ff2e7fc242caf8
Hhajar21 <hajarhabibi016@gmail>
2023-10-04 22:39:56 +0200

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python3
-"""fetches information from JSONplaceholder API and exports to JSON"""
+"""Fetches information from JSONplaceholder API and exports to JSON"""
 
-from json import dump
-from requests import get
-from sys import argv
+import json
+import requests
+import sys
 
+def export_to_json(user_id):
+    main_url = "https://jsonplaceholder.typicode.com"
+    todo_url = f"{main_url}/users/{user_id}/todos"
+    user_url = f"{main_url}/users/{user_id}"
+
+    try:
+        # Fetch todo data
+        todo_response = requests.get(todo_url)
+        todo_response.raise_for_status()  # Raise an exception for HTTP errors
+        todo_data = todo_response.json()
+
+        # Fetch user data
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        user_data = user_response.json()
+
+        if not todo_data:
+            print("No tasks found for this user.")
+            return
+
+        # Create a dictionary with the desired format
+        user_dict = {
+            user_id: [{"task": todo["title"], "completed": todo["completed"], "username": user_data["username"]} for todo in todo_data]
+        }
+
+        # Export the data to a JSON file
+        filename = f"{user_id}.json"
+        with open(filename, 'w') as json_file:
+            json.dump(user_dict, json_file, indent=4)
+
+        print(f"Data exported to {filename}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
-        argv[1])
-    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
-
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}
-        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
-            "completed"), "username": name_result.get("username")})
-        todo_list.append(todo_dict)
-
-    with open("{}.json".format(argv[1]), 'w') as f:
-        dump({argv[1]: todo_list}, f)
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py USER_ID")
+    else:
+        user_id = sys.argv[1]
+        export_to_json(user_id)
