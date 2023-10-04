0dea7d54bbec828faea98a3c94e6977e2293e5b5
Hhajar21 <hajarhabibi016@gmail>
2023-10-04 22:04:42 +0200

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,21 +1,4 @@
 #!/usr/bin/python3
-"""fetches information from JSONplaceholder API and exports to CSV"""
-
-from csv import DictWriter, QUOTE_ALL
-from requests import get
-from sys import argv
-
-
-if __name__ == "__main__":
-    main_url = "https://jsonplaceholder.typicode.com"
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
-
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}#!/usr/bin/python3
 """Fetches information from JSONplaceholder API and exports to CSV"""
 
 import csv
@@ -25,33 +8,39 @@ import sys
 def export_to_csv(user_id):
     main_url = "https://jsonplaceholder.typicode.com"
     todo_url = f"{main_url}/users/{user_id}/todos"
-    name_url = f"{main_url}/users/{user_id}"
-    
+    user_url = f"{main_url}/users/{user_id}"
+
     try:
         todo_response = requests.get(todo_url)
         todo_response.raise_for_status()  # Raise an exception for HTTP errors
-        
-        name_response = requests.get(name_url)
-        name_response.raise_for_status()
-        
+
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+
         todo_data = todo_response.json()
-        name_data = name_response.json()
-        
+        user_data = user_response.json()
+
         # Create a list of dictionaries with the desired data
-        todo_list = [{"USER_ID": user_id, "USERNAME": name_data["username"],
-                      "TASK_COMPLETED_STATUS": str(todo["completed"]),
-                      "TASK_TITLE": todo["title"]} for todo in todo_data]
-        
+        todo_list = []
+        for todo in todo_data:
+            todo_dict = {
+                "USER_ID": user_id,
+                "USERNAME": user_data["username"],
+                "TASK_COMPLETED_STATUS": str(todo["completed"]),
+                "TASK_TITLE": todo["title"]
+            }
+            todo_list.append(todo_dict)
+
         # Export the data to a CSV file
-        with open(f"{user_id}.csv", 'w', newline='') as csv_file:
+        filename = f"{user_id}.csv"
+        with open(filename, 'w', newline='') as csv_file:
             fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-            
             writer.writeheader()  # Write the header row
             writer.writerows(todo_list)  # Write the data
-        
-        print(f"Data exported to {user_id}.csv")
-    
+
+        print(f"Data exported to {filename}")
+
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
     except Exception as e:
@@ -63,12 +52,3 @@ if __name__ == "__main__":
     else:
         user_id = sys.argv[1]
         export_to_csv(user_id)
-
-        todo_dict.update({"user_ID": argv[1], "username": name_result.get(
-            "username"), "completed": todo.get("completed"),
-                          "task": todo.get("title")})
-        todo_list.append(todo_dict)
-    with open("{}.csv".format(argv[1]), 'w', newline='') as f:
-        header = ["user_ID", "username", "completed", "task"]
-        writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
-        writer.writerows(todo_list)
