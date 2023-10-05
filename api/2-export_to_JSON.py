b92c0c07c2c030eabf21d5c077707eb10dff307c
Hhajar21 <hajarhabibi016@gmail>
2023-10-05 16:14:47 +0200

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
 """fetches information from JSONplaceholder API and exports to JSON"""
 
-import json
-import requests
-import sys
+from json import dump
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    try:
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
+            "completed"), "username": name_result.get("username")})
+        todo_list.append(todo_dict)
 
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-
-        todo_data = todo_response.json()
-        user_data = user_response.json()
-
-        todo_list = []
-        for todo in todo_data:
-            todo_dict = {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": user_data["username"],
-            }
-            todo_list.append(todo_dict)
-
-        with open(f"{user_id}.json", "w") as json_file:
-            json.dump({user_id: todo_list}, json_file, indent=4)
-
-        print(f"Data exported to {user_id}.json")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open("{}.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: todo_list}, f)
