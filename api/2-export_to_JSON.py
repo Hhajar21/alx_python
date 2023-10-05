474f8fd349083f1361461e12aef06f4d8995ed8b
Hhajar21 <hajarhabibi016@gmail>
2023-10-05 15:55:29 +0200

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,12 +5,7 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py USER_ID")
-        sys.exit(1)
-
-    user_id = sys.argv[1]
+def export_to_json(user_id):
     main_url = "https://jsonplaceholder.typicode.com"
     todo_url = f"{main_url}/users/{user_id}/todos"
     user_url = f"{main_url}/users/{user_id}"
@@ -26,15 +21,20 @@ if __name__ == "__main__":
         user_data = user_response.json()
 
         task_list = [{"task": task["title"], "completed": task["completed"], "username": user_data["username"]} for task in todo_data]
-        
+
         with open(f"{user_id}.json", "w") as json_file:
-            json.dump({user_id: task_list}, json_file)
+            json.dump({user_id: task_list}, json_file, indent=4)  # Indent for pretty formatting
 
         print(f"Data exported to {user_id}.json")
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
-        sys.exit(1)
     except Exception as e:
         print(f"An error occurred: {e}")
-        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 2-export_to_JSON.py USER_ID")
+    else:
+        user_id = sys.argv[1]
+        export_to_json(user_id)
