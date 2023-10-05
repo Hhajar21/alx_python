58d44a8eecb951352c7c9634832bdd823c52867a
Hhajar21 <hajarhabibi016@gmail>
2023-10-05 15:50:16 +0200

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-"""fetches information from JSONplaceholder API and exports to JSON"""
+"""Fetches information from JSONplaceholder API and exports to JSON"""
 
-from json import dump
-from requests import get
-from sys import argv
-
+import json
+import requests
+import sys
 
 if __name__ == "__main__":
-    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
-        argv[1])
-    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    if len(sys.argv) != 2:
+        print("Usage: python3 2-export_to_JSON.py USER_ID")
+        sys.exit(1)
 
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}
-        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
-            "completed"), "username": name_result.get("username")})
-        todo_list.append(todo_dict)
+    user_id = sys.argv[1]
+    main_url = "https://jsonplaceholder.typicode.com"
+    todo_url = f"{main_url}/users/{user_id}/todos"
+    user_url = f"{main_url}/users/{user_id}"
 
-    with open("{}.json".format(argv[1]), 'w') as f:
-        dump({argv[1]: todo_list}, f)
+    try:
+        todo_response = requests.get(todo_url)
+        todo_response.raise_for_status()
+
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+
+        todo_data = todo_response.json()
+        user_data = user_response.json()
+
+        task_list = [{"task": task["title"], "completed": task["completed"], "username": user_data["username"]} for task in todo_data]
+        
+        with open(f"{user_id}.json", "w") as json_file:
+            json.dump({user_id: task_list}, json_file)
+
+        print(f"Data exported to {user_id}.json")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
